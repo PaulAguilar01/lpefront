2bb2051dce7899bc8d3e93aa30cc7f6925ec8b49
Paul Aguilar <christianpaz@ext.inditex.com>
2020-06-01 15:42:08 -0500

    Creados DockerFile y .circleci

--- src/App.js
+++ src/App.js
@@ -8,16 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HELLO WORLD
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>
+          ThoughtWorks
+        </h3>
       </header>
     </div>
   );
